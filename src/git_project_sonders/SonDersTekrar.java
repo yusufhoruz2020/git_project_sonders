76a3e1af5220aa8a1f1c0ac61c85ef2f666949f7
Yusuf <yusufhoruz46066@gmail.com>
2020-08-16 22:00:26 +0200

    version3

--- src/git_project_sonders/SonDersTekrar.java
+++ src/git_project_sonders/SonDersTekrar.java
@@ -1,10 +1,10 @@
 package git_project_sonders;
 
-public class OrtakCalisma {
+public class SonDersTekrar {
 
 	public static void main(String[] args) {
+		System.out.println("21:55 master branch");
 		
-		System.out.println("2020 github..");
 
 	}
 
