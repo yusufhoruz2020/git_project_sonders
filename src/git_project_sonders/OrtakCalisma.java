4b756aadb4b52fe6448bfa0a65fc4267cf06e9d9
Yusuf <yusufhoruz46066@gmail.com>
2020-08-16 21:49:18 +0200

    version1

--- src/git_project_sonders/OrtakCalisma.java
+++ src/git_project_sonders/OrtakCalisma.java
@@ -3,7 +3,8 @@ package git_project_sonders;
 public class OrtakCalisma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("2020 github..");
 
 	}
 
