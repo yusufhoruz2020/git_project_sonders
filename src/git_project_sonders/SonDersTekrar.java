3d8447cbca769255485324cd685ee40014d9cad9
Yusuf <yusufhoruz46066@gmail.com>
2020-08-16 22:18:33 +0200

    feature 2. commit

--- src/git_project_sonders/SonDersTekrar.java
+++ src/git_project_sonders/SonDersTekrar.java
@@ -5,7 +5,7 @@ public class SonDersTekrar {
 	public static void main(String[] args) {
 		System.out.println("21:55 master branch");
 		System.out.println("22:05 feature branch");
-		
+		System.out.println("22:17 feature branch");
 
 	}
 
